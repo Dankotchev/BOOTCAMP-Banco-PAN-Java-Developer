dcc9315f17d0809d3ccbdd80d08bfe48fab4d4c7
Dankotchev <dankotchev.tech@outlook.com>
2023-04-29 15:15:03 -0300

    Desafio de Projeto - Padrões de Projeto -- Model e Repository

---
+++ Desafios_de_Projeto/PadrõesDeProjeto/src/main/java/edu/dio/padroesprojeto/model/Cliente.java
@@ -0,0 +1,25 @@
+package edu.dio.padroesprojeto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "cliente", length = 60, nullable = false)
+    private String nome;
+
+    @ManyToOne
+    private Endereco endereco;
+}
---
+++ Desafios_de_Projeto/PadrõesDeProjeto/src/main/java/edu/dio/padroesprojeto/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package edu.dio.padroesprojeto.repository;
+
+import edu.dio.padroesprojeto.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
